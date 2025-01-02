239fadfe4671fd305f85304b39a6b0e1dc5f65bb
Sanskar Nenawati <98727743+sanskarnenawati@users.noreply.github.com>
2025-01-02 17:50:17 +0530

    Update app.py
    bug fixing

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Define questions and responses
 qa_pairs = {
